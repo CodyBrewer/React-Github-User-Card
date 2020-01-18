4b31ec9b18bb34d52000f4aa3fa47a02a3620440
CodyBrewer <brewer.cody.m@gmail.com>
2020-01-18 14:47:32 -0700

    set up taiwind gradients

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -4,24 +4,30 @@ import "./styles/styles.css";
 import GithubUser from "./components/UserComponents/GithubUser";
 import Followers from "./components/FollowersComponents/Followers";
 import Following from "./components/FollowingComponents/Following";
+import Search from "./components/SearchComponents/Search";
 
 class App extends Component {
   state = {
     user: "CodyBrewer",
     userData: [],
-    followersData: [],
-    followingData: []
+    followingData: [],
+    search: ""
   };
 
   componentDidMount() {
     this.getGithubData();
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log(this.state);
+    if (prevState.user !== this.state.user) {
+      this.getGithubData();
+    }
+  }
+
   // - [ ] Fetch data from the Github API for a Github user
   getGithubData = () => {
     this.getUserData(this.state.user);
-    this.getFollowerData(this.state.user);
-    this.getFollowingData(this.state.user);
   };
 
   getUserData = user => {
@@ -36,17 +42,17 @@ class App extends Component {
       .catch(err => console.log(err.message));
   };
 
-  getFollowerData = user => {
-    fetch(`https://api.github.com/users/${user}/followers`)
-      .then(res => res.json())
-      .then(githubData => {
-        this.setState({
-          ...this.state,
-          followersData: githubData
-        });
-      })
-      .catch(err => console.log(err.message));
-  };
+  // getFollowerData = user => {
+  //   fetch(`https://api.github.com/users/${user}/followers`)
+  //     .then(res => res.json())
+  //     .then(githubData => {
+  //       this.setState({
+  //         ...this.state,
+  //         followersData: githubData
+  //       });
+  //     })
+  //     .catch(err => console.log(err.message));
+  // };
 
   getFollowingData = user => {
     fetch(`https://api.github.com/users/${user}/following`)
@@ -60,13 +66,41 @@ class App extends Component {
       .catch(err => console.log(err.message));
   };
 
+  handleSearchUser = user => {
+    this.setState({
+      user: user
+    });
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSearchSubmit = e => {
+    e.preventDefault();
+    this.handleSearchUser(this.state.search);
+    this.setState({ search: "" });
+  };
+
   render() {
     return (
-      <div className="container mx-auto">
+      <div className="container mx-auto bg-gradient-tr-green-blue">
+        <Search
+          handleSearchSubmit={this.handleSearchSubmit}
+          handleChange={this.handleChange}
+          search={this.state.search}
+        />
         <GithubUser userData={this.state.userData} />
         <section className="w-11/12 mx-auto flex">
-          <Followers followersData={this.state.followersData} />
-          <Following followingData={this.state.followingData} />
+          <Followers
+            user={this.state.user}
+            followersData={this.state.followersData}
+          />
+          <Following
+            user={this.state.user}
+            getFollowingData={this.getFollowingData}
+            followingData={this.state.followingData}
+          />
         </section>
       </div>
     );
--- react-github-user-card/tailwind.config.js
+++ react-github-user-card/tailwind.config.js
@@ -1,7 +1,137 @@
 module.exports = {
   theme: {
-    extend: {}
+    linearGradients: {
+      directions: {
+        // defaults to these values
+        t: "to top",
+        tr: "to top right",
+        r: "to right",
+        br: "to bottom right",
+        b: "to bottom",
+        bl: "to bottom left",
+        l: "to left",
+        tl: "to top left"
+      },
+      colors: {
+        // defaults to {}
+        red: "#f00",
+        "red-blue": ["#f00", "#00f"],
+        "red-green-blue": ["#f00", "#0f0", "#00f"],
+        "black-white-with-stops": ["#000", "#000 45%", "#fff 55%", "#fff"],
+        "green-blue": ["#0f0", "#00f"]
+      }
+    },
+    radialGradients: {
+      shapes: {
+        // defaults to this value
+        default: "ellipse"
+      },
+      sizes: {
+        // defaults to this value
+        default: "closest-side"
+      },
+      positions: {
+        // defaults to these values
+        default: "center",
+        t: "top",
+        tr: "top right",
+        r: "right",
+        br: "bottom right",
+        b: "bottom",
+        bl: "bottom left",
+        l: "left",
+        tl: "top left"
+      },
+      colors: {
+        // defaults to {}
+        red: "#f00",
+        "red-blue": ["#f00", "#00f"],
+        "red-green-blue": ["#f00", "#0f0", "#00f"],
+        "black-white-with-stops": ["#000", "#000 45%", "#fff 55%", "#fff"]
+      }
+    },
+    conicGradients: {
+      startingAngles: {
+        // defaults to this value
+        default: "0"
+      },
+      positions: {
+        // defaults to these values
+        default: "center",
+        t: "top",
+        tr: "top right",
+        r: "right",
+        br: "bottom right",
+        b: "bottom",
+        bl: "bottom left",
+        l: "left",
+        tl: "top left"
+      },
+      colors: {
+        // defaults to {}
+        red: "#f00",
+        "red-blue": ["#f00", "#00f"],
+        "red-green-blue": ["#f00", "#0f0", "#00f"],
+        checkerboard: [
+          "white 90deg",
+          "black 90deg 180deg",
+          "white 180deg 270deg",
+          "black 270deg"
+        ]
+      }
+    },
+    repeatingLinearGradients: theme => ({
+      directions: theme("linearGradients.directions"), // defaults to the same values as linearGradients’ directions
+      colors: theme("linearGradients.colors"), // defaults to {}
+      lengths: {
+        // defaults to {}
+        sm: "25px",
+        md: "50px",
+        lg: "100px"
+      }
+    }),
+    repeatingRadialGradients: theme => ({
+      shapes: theme("radialGradients.shapes"), // defaults to the same value as radialGradients’ shapes
+      sizes: {
+        // defaults to this value
+        default: "farthest-corner"
+      },
+      positions: theme("radialGradients.positions"), // defaults to the same values as radialGradients’ positions
+      colors: theme("radialGradients.colors"), // defaults to {}
+      lengths: {
+        // defaults to {}
+        sm: "25px",
+        md: "50px",
+        lg: "100px"
+      }
+    }),
+    repeatingConicGradients: theme => ({
+      startingAngles: theme("conicGradients.startingAngles"), // defaults to the same value as conicGradients’ startingAngles
+      positions: theme("conicGradients.positions"), // defaults to the same values as conicGradients’ positions
+      colors: {
+        // defaults to {}
+        red: "#f00",
+        "red-blue": ["#f00", "#00f"],
+        "red-green-blue": ["#f00", "#0f0", "#00f"],
+        starburst: ["white 0 5deg", "blue 5deg"]
+      },
+      lengths: {
+        // defaults to {}
+        sm: "10deg",
+        md: "20deg",
+        lg: "40deg"
+      }
+    })
   },
-  variants: {},
-  plugins: []
-}
+  variants: {
+    // all the following default to ['responsive']
+    backgroundImage: ["responsive"], // this is for the "bg-none" utility
+    linearGradients: ["responsive"],
+    radialGradients: ["responsive"],
+    conicGradients: ["responsive"],
+    repeatingLinearGradients: ["responsive"],
+    repeatingRadialGradients: ["responsive"],
+    repeatingConicGradients: ["responsive"]
+  },
+  plugins: [require("tailwindcss-gradients")()]
+};
